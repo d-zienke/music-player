b880da7214750e5ad2fe86b3ec4bf0a796d36af9
Damian Zienke <zienke.damian@gmail.com>
2023-05-23 20:24:44 +0200

    Add base files

---
+++ main.js
@@ -0,0 +1,2 @@
+import data from "./src/data";
+const songsArray = data;
---
+++ src/data.js
@@ -0,0 +1,92 @@
+export default [
+	{
+		trackId: "1",
+		title: "Magic Escape Room",
+		author: "Kevin MacLeod",
+		trackPath: "music/track-1.mp3",
+		img: {
+			author: {
+				name: "Javier Miranda",
+				link: "https://unsplash.com/@nuvaproductions?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			},
+			source: "Unsplash",
+			link: "https://unsplash.com/photos/xB2XP29gn10?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			imgPath: "images/img-1.jpg",
+		},
+		source: "incompetech.com",
+		license:
+			"Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/",
+	},
+	{
+		trackId: "2",
+		title: "The Entertainer",
+		author: "Kevin MacLeod",
+		trackPath: "music/track-2.mp3",
+		img: {
+			author: {
+				name: "Milad Fakurian",
+				link: "https://unsplash.com/@fakurian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			},
+			source: "Unsplash",
+			link: "https://unsplash.com/photos/PGdW_bHDbpI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			imgPath: "images/img-2.jpg",
+		},
+		source: "incompetech.com",
+		license:
+			"Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/",
+	},
+	{
+		trackId: "3",
+		title: "Maple Leaf Rag",
+		author: "Kevin MacLeod",
+		trackPath: "music/track-3.mp3",
+		img: {
+			author: {
+				name: "Esther Jiao",
+				link: "https://unsplash.com/@estherrj?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			},
+			source: "Unsplash",
+			link: "https://unsplash.com/photos/B3FyeM21GdA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			imgPath: "images/img-3.jpg",
+		},
+		source: "incompetech.com",
+		license:
+			"Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/",
+	},
+	{
+		trackId: "4",
+		title: "Amazing Plan",
+		author: "Kevin MacLeod",
+		trackPath: "music/track-4.mp3",
+		img: {
+			author: {
+				name: "Ricardo Gomez Angel",
+				link: "https://unsplash.com/@rgaleriacom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			},
+			source: "Unsplash",
+			link: "https://unsplash.com/photos/3kzlCL3rj8A?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			imgPath: "images/img-4.jpg",
+		},
+		source: "incompetech.com",
+		license:
+			"Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/",
+	},
+	{
+		trackId: "5",
+		title: "Olde Timey",
+		author: "Kevin MacLeod",
+		trackPath: "music/track-5.mp3",
+		img: {
+			author: {
+				name: "Denys Nevozhai",
+				link: "https://unsplash.com/@dnevozhai?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			},
+			source: "Unsplash",
+			link: "https://unsplash.com/photos/w7YCquMkv2c?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+			imgPath: "images/img-5.jpg",
+		},
+		source: "incompetech.com",
+		license:
+			"Licensed under Creative Commons: By Attribution 4.0 License http://creativecommons.org/licenses/by/4.0/",
+	},
+];
